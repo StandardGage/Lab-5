a24772aa87206eb1db10e9a724a3124c3efe15ec
faithlovell <faithlxvell@gmail.com>
2022-05-02 18:53:56 -0400

    problem 6 done

--- app/src/main/java/Lab5/Recruiter.java
+++ app/src/main/java/Lab5/Recruiter.java
@@ -1,4 +1,7 @@
 package Lab5;
 
 public interface Recruiter {
+    public abstract int getNumRecruits();
+    public abstract void setNumRecruits(int numRecruits);
+    public abstract boolean validRecruitPath(int rowRecruiter, int columnRecruiter, int rowRecruit, int columnRecruit);
 }
