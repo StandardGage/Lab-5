d8a26dcee1a8f3530ac6c64807d4f297cb55bd93
faithlovell <faithlxvell@gmail.com>
2022-05-02 20:03:48 -0400

    problem 7 done

--- app/src/main/java/Lab5/ActionMove.java
+++ app/src/main/java/Lab5/ActionMove.java
@@ -1,4 +1,19 @@
 package Lab5;
 
-public class ActionMove {
+public class ActionMove extends Action{
+    //represent an action the user can take where they move their own piece from the first square
+    //on the board to the second square on the board
+
+    public ActionMove(GameS22 game, int fromRow, int fromColumn, int toRow, int toColumn){
+        super(game, fromRow, fromColumn, toRow, toColumn);
+    }
+
+    @Override
+    public void performAction() {
+        Piece fromPiece = game.getBoardSquares()[fromRow][fromColumn].getPiece();
+        fromPiece.speak();
+        game.getBoardSquares()[fromRow][fromColumn].removePiece();
+        game.getBoardSquares()[toRow][toColumn].setPiece(fromPiece);
+        game.changeTurn();
+    }
 }
