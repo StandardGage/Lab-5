666cb955923b397cc9a3d11c5cf0f8e7f069b2eb
faithlovell <faithlxvell@gmail.com>
2022-05-02 22:19:55 -0400

    finished the god awful rules conditions

--- app/src/main/java/Lab5/Rules.java
+++ app/src/main/java/Lab5/Rules.java
@@ -1,4 +1,108 @@
 package Lab5;
 
 public class Rules {
+
+    public static boolean checkValidAction(GameS22 game, int fromRow, int fromColumn, int toRow, int toColumn, char actionType) {
+        BoardSquare fromSquare = game.getBoardSquares()[fromRow][fromColumn];
+        BoardSquare toSquare = game.getBoardSquares()[toRow][toColumn];
+        Piece fromPiece = fromSquare.getPiece();
+        boolean validAction = false;
+        boolean toFromBounds = false;
+        boolean fromIsCurrentTeam = false;
+        boolean validPath = false;
+
+        //checks four conditions necessary for each rule: indexes in bounds, from piece exists & belongs to current team,
+        // & to-from path is valid.
+        if (game.getGameBoard().inBounds(fromRow, fromColumn) || game.getGameBoard().inBounds(toRow, toColumn)) {
+            toFromBounds = true;
+        }
+        if (!(fromSquare.isEmpty()) && fromPiece.getTeamColor().equals(game.getCurrentTeam().getTeamColor())) {
+            fromIsCurrentTeam = true;
+        }
+        if(fromPiece.validMovePath(fromRow, fromColumn, toRow, toColumn)) { //path is valid
+            validPath = true;
+        }
+            if (toFromBounds && fromIsCurrentTeam && validPath) {
+            switch (actionType) {
+                //I split a lot of the if statements up in the cases just for readability
+                //ex I know in case S I could combine it to (fromPiece.canSpawn() && toSquare.isEmpty)
+                //was just easier to do so we can make sure all rules are covered step-by-step
+                case 'M':
+                    if(toSquare.isEmpty()){ //no piece on to square
+                        validAction = true;
+                    }
+                    break;
+
+                case 'S':
+                    //check if piece can spawn
+                    if(fromPiece.canSpawn()){
+                        //checks to square is empty
+                        if(toSquare.isEmpty()){
+                            validAction = true;
+                        }
+                    }
+                    break;
+
+                case 'R':
+                    //check if piece is recruiter
+                    if(fromPiece instanceof Recruiter) {
+                        //check if valid recruit path
+                        if (((Recruiter) fromPiece).validRecruitPath(fromRow, fromColumn, toRow, toColumn)) {
+                            //check if toSquare contains piece
+                            if (!(toSquare.isEmpty())) {
+                                //check if toSquare piece belongs to Opponent team
+                                if (toSquare.getPiece().getTeamColor().equals(game.getOpponentTeam().getTeamColor())) {
+                                    validAction = true;
+                                }
+                            }
+                        }
+                    }
+                    break;
+
+                case 'A':
+                    //check if piece on toSquare
+                    if(!(toSquare.isEmpty())) {
+                        //check if piece is attacker
+                        if (fromPiece instanceof Attacker) {
+                            //checks if there is a valid attack path
+                            if (((Attacker) fromPiece).validAttackPath(fromRow, fromColumn, toRow, toColumn)) {
+                                //checks if to piece is opponent piece
+                                if (toSquare.getPiece().getTeamColor().equals(game.getOpponentTeam().getTeamColor())) {
+                                    //all attackers can attack opponents, so:
+                                    //PieceBlueHen can automatically attack opponent
+                                    if (fromPiece instanceof PieceBlueHen) {
+                                        validAction = true;
+                                    }
+                                    //PieceBuzz working laser
+                                    if (fromPiece instanceof PieceBuzz) {
+                                        if (((PieceBuzz) fromPiece).canAttack()) {
+                                            validAction = true;
+                                        }
+                                    }
+                                    //PieceEvilMinion hungry
+                                    if (fromPiece instanceof PieceEvilMinion) {
+                                        if (((PieceEvilMinion) fromPiece).canAttack()) {
+                                            validAction = true;
+                                        }
+                                    }
+                                }
+                                else{
+                                    //if to piece is current team, evil minion may attack...
+                                    if(fromPiece instanceof PieceEvilMinion){
+                                        if (((PieceEvilMinion) fromPiece).canAttack()){
+                                            //...if to piece is a minion
+                                            if(toSquare.getPiece() instanceof PieceMinion){
+                                                validAction = true;
+                                            }
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
+                    break;
+            }
+        }
+        return validAction;
+    }
 }
